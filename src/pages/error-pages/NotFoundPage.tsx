e7ac352500c46dd1f494f9556b083fcc33c13a9d
abdullahMohamed13 <https://github.com/abdullahMohamed13>
2025-08-07 17:42:14 +0300

    Update 404 page UI

--- src/pages/error-pages/NotFoundPage.tsx
+++ src/pages/error-pages/NotFoundPage.tsx
@@ -1,10 +1,38 @@
+import { Box, Button, Container } from "@mui/material";
+import HeaderComponent from "../../components/HeaderComponent";
+
 export function NotFoundPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-background text-text p-6">
-      <div className="text-center max-w-md">
-        <h2 className="text-4xl font-bold text-primary mb-4">404 - Page Not Found</h2>
-        <p className="text-lg">Hmm, the page you were looking for doesn't seem to exist anymore. LMAO 🤣</p>
-      </div>
-    </div>
+    <Container className="min-h-screen flex items-center justify-center bg-background text-text p-6">
+      <Box className="text-center max-w-md">
+        <img
+          src="/images/just-brand-logo.webp"
+          className="rounded-2xl mb-3 mx-auto"
+          width={170}height={170}
+          alt="Website logo brand" />
+          <HeaderComponent
+            headingText="404 - Page Not Found"
+            subHeadingText="Hmm, the page you were looking for doesn't seem to exist anymore. LMAO 🤣"
+            />
+        <Button 
+          variant="contained" 
+          size="large"
+          sx={{ 
+            px: 4, 
+            py: 2,
+            fontSize: '1.1rem',
+            fontWeight: 700,
+            background: 'linear-gradient(45deg, #e20000, #ff6b35)',
+            '&:hover': {
+              background: 'linear-gradient(45deg, #c10000, #e55a2b)',
+              transform: 'scale(1.05)'
+            },
+            transition: 'all 0.3s ease'
+          }}
+        >
+          <a href='/'>GO HOME</a>
+        </Button>
+      </Box>
+    </Container>
   );
 }
